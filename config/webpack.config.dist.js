9e5a1efca1fa40ecdec7e56c652bff9e33a594d3
johnrhampton <johnrhampton@gmail.com>
2016-08-04 16:10:53 -0400

    define NODE_ENV in dist webpack

--- config/webpack.config.dist.js
+++ config/webpack.config.dist.js
@@ -86,6 +86,10 @@ module.exports = {
         comments: false,
         screw_ie8: true
       }
+    }),
+
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"'
     })
   ]
 };
