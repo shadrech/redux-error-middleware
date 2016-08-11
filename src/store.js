6071f5a07c63ac01a4b348997c8c08980b6184fe
johnrhampton <johnrhampton@gmail.com>
2016-08-10 21:01:59 -0400

    adding handleError function in store sandbox

--- src/store.js
+++ src/store.js
@@ -2,12 +2,21 @@ import {createStore, applyMiddleware, compose} from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
 import errorMiddleware from '../lib/index';
-
 import rootReducer from './reducer';
 
 export default (initial_state) => {
+  /**
+   * example of an action creator
+   */
+  const handleError = (error) => {
+    return (dispatch, getState) => {
+      console.log(error.message);
+      dispatch({type: 'HANDLED_ERROR'});
+    };
+  };
+
   const actions = {
-    error: [{type: 'HANDLE_ERROR', text: 'ERROR'}],
+    error: [{type: 'HANDLE_ERROR', text: 'ERROR'}, handleError],
     forbidden: [{type: 'FORBIDDEN_ERROR', text: 'FORBIDDEN'}]
   };
 
